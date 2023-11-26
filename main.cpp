e8d9071c3c3403c60f319bd994bf6702384f67e7
germanFid <german.fidarov@gmail.com>
2023-11-27 01:49:00 +0300

    add insert()

--- main.cpp
+++ main.cpp
@@ -2,47 +2,81 @@
 
 struct Vertex
 {
-    char symbol;
     std::map<char, struct Vertex*> next;
+    bool is_terminal;
+    struct Vertex* parent;
+
+    // for auto
+    // struct Vertex* sufflink;
+    // struct Vertex** go;
+
 };
 
 
-class trie
+class Trie
 {
 private:
     struct Vertex* root;
     
-    struct Vertex* initVertex();
-    struct Vertex* initVertex(char c);
+    struct Vertex* initVertex(struct Vertex* parent);
 
 public:
-    trie();
-    ~trie();
+    Trie();
+    ~Trie();
+
+    void insert(char* cstr);
+    void find();
 };
 
-Vertex *trie::initVertex()
+//
+// Check https://www.cs.usfca.edu/~galles/visualization/Trie.html
+//
+Vertex *Trie::initVertex(struct Vertex* parent)
 {
-    struct Vertex* result = new Vertex();
-    result->symbol = 0;
+    struct Vertex* result = new struct Vertex();
+
     result->next = std::map<char, struct Vertex*>();
+    result->is_terminal = false;
+    result->parent = parent;
 
     return result;
 }
 
-Vertex *trie::initVertex(char c)
+Trie::Trie()
 {
-    struct Vertex* result = initVertex();
-    result->symbol = c;
-
-    return result;
+    root = initVertex(nullptr);
 }
 
-trie::trie()
+Trie::~Trie()
 {
-    
+
 }
 
-trie::~trie()
+void Trie::insert(char* cstr)
 {
+    struct Vertex* current = this->root;
+    char* ptr = cstr;
+    while (*ptr)
+    {
+        if (auto search = current->next.find(*ptr); search != current->next.end())
+        {
+            ptr++;
+            current = search->second;
+            continue;
+        }
 
+        current->next[*ptr] = initVertex(current);
+        current = current->next[*ptr];
+        ptr++;
+    }
+
+    current->is_terminal = true;
+}
+
+int main()
+{
+    Trie t;
+    t.insert("abc");
+    t.insert("ac");
+    return 0;
 }
